74df508503ca53985b1a973d692094a4448a7b53
w1llow1sp <mari.front.74@gmail.com>
2023-03-07 17:57:16 +0500

    Added solution of miscrotasks(monday,second week): usage universal components & work w/ global or local useState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
+import {FullInput} from "./components/FullInput";
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   let [message, setMessage] = useState([
+        {message:'message1'},
+        {message:'message2'},
+        {message:'message3'},
+    ]);
+
+    let [title, setTitle] = useState('')
+    console.log(title)
+
+/*    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        //console.log(event.currentTarget.value)
+        setTitle(event.currentTarget.value)
+    }*/
+
+    const addMessage = (title:string) => {
+        let newMessage ={message:title}
+        setMessage([newMessage,...message])
+    }
+
+    const callBackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+
+    }
+
+/*
+   const ButtonChangeSender= (title:string) => {
+       //console.log(title)
+       let newMessage = {message:title}
+       setMessage([newMessage,...message])
+   }
+*/
+
+
+    return (
+        <div className={'App'}>
+            <Input setTitle={setTitle} title={title} />
+            <Button name={'+'} callBack={callBackButtonHandler}/>
+            {/*<FullInput functionForBtn={ButtonChangeSender}/>*/}
+{/*            <div>
+                <input />
+                <button>+</button>
+            </div>*/}
+            {message.map((el,index)=>{
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
   );
 }
 
